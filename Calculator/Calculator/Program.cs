0ff231fb64fec7ce60eb8427f6a03263e2517f4c
proh368 <ivan.chistyakov.13@gmail.com>
2024-09-13 23:01:54 +0400

    version for umnozhenie

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите множитель a:");
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите множитель b:");
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{a} * {b} = {a * b}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Введены неверные данные");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        
+    }
+}
